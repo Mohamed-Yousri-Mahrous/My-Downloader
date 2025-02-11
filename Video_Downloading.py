430b41a137fcbea5fc004584701e8b063224310d
Mohamed Yousri <mohamed.yousri.mahrous@gmail.com>
2025-02-11 17:44:37 +0200

    update video_downloading function to use class

--- Video_Downloading.py
+++ Video_Downloading.py
@@ -2,120 +2,128 @@ import os
 import yt_dlp
 
 
-def Welcome():
-    os.system("cls" if os.name == "nt" else "clear")
-    print("=" * 80)
-    print(" Welcome to the Video Downloading program ".center(80, "=").title())
-    print(" Created By Mohamed Yousri ".center(80, "=").title())
-    print("=" * 80, end="\n\n")
+class Video:
+    def __init__(self):
+        self.download_path = os.path.expanduser(r"~\Downloads")
 
+    def Welcome(self):
+        os.system("cls" if os.name == "nt" else "clear")
+        print("=" * 80)
+        print(" Welcome to the Video Downloading program ".center(80, "=").title())
+        print(" Created By Mohamed Yousri ".center(80, "=").title())
+        print("=" * 80, end="\n\n")
 
-def convert_duration(seconds):
+    def convert_duration(self, seconds):
+        hours = seconds // 3600
+        minutes = (seconds % 3600) // 60
+        seconds = seconds % 60
 
-    hours = seconds // 3600
-    minutes = (seconds % 3600) // 60
-    seconds = seconds % 60
+        if hours == 0:
+            return f"{minutes}m {seconds}s"
+        elif minutes == 0:
+            return f"{seconds}s"
+        else:
+            return f"{hours}h {minutes}m {seconds}s"
 
-    if hours == 0:
-        return f"{minutes}m {seconds}s"
-    elif minutes == 0:
-        return f"{seconds}s"
-    else:
-        return f"{hours}h {minutes}m {seconds}s"
+    def get_info(self, url):
+        try:
+            url_option = {
+                "quiet": True,
+                "no_warnings": True,
+            }
 
+            with yt_dlp.YoutubeDL(url_option) as youtube_Video:
+                self.video_info = youtube_Video.extract_info(url, download=False)
 
-def get_info(url):
+            # Display the playlist information in the Terminal
+            video_title = self.video_info["title"]
+            Youtube_channel = self.video_info["channel"]
+            video_duration = self.video_info["duration"]
+            video_duration = self.convert_duration(video_duration)
 
-    try:
+            print(
+                f" - You are downloading Video >> {video_title}".title(),
+                end="\n\n",
+            )
+            print(
+                f" - From ({Youtube_channel}) Channel on YouTube".title(),
+                end="\n\n",
+            )
+            print(f" - The Video Duration is >> {video_duration}".title(), end="\n\n")
 
-        url_option = {
-            "quiet": True,
-            "no_warnings": True,
-        }
+            print(f" - The Video will be Saved in >> {self.download_path}", end="\n\n")
 
-        with yt_dlp.YoutubeDL(url_option) as youtube_Video:
-            video_info = youtube_Video.extract_info(url, download=False)
+            print(
+                f" Now downloading the Video ... ".center(80, "=").title(), end="\n\n"
+            )
 
-        # Display the playlist information in the Terminal
-        video_title = video_info["title"]
-        Youtube_channel = video_info["channel"]
-        video_duration = video_info["duration"]
-        video_duration = convert_duration(video_duration)
+            return self.video_info
 
-        print(f" - You are downloading Video >> {video_title}".title(), end="\n\n")
-        print(f" - From ({Youtube_channel}) Channel on YouTube".title(), end="\n\n")
-        print(f" - The Video Duration is >> {video_duration}".title(), end="\n\n")
+        except KeyError as ke:
+            print(f"KeyError: {ke}")
+            input("Press Enter to Continue...")
 
-        download_path = os.path.expanduser(r"~\Downloads")
-        print(f" - The Video will be Saved in >> {download_path}", end="\n\n")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            input("Press Enter to Continue...")
 
-        print(f" Now downloading the Video ... ".center(80, "=").title(), end="\n\n")
+    def download_video(self):
+        if not self.video_info:
+            print("No video information available. Cannot download.")
+            input("Press Enter to Continue...")
+            return
 
-        return video_info
+        try:
+            video_option = {
+                "format": "best",
+                "outtmpl": os.path.join(
+                    os.path.expanduser("~/Downloads"), "%(title)s.%(ext)s"
+                ),
+                "quiet": True,
+                "no_warnings": True,
+            }
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        input("Press Enter to Continue...")
+            print(f"Downloading {self.video_info['title']} ...", end="\n\n")
 
+            with yt_dlp.YoutubeDL(video_option) as video_download:
+                video_download.download([self.video_info["webpage_url"]])
 
-def download_video(video_info):
-    if not video_info:
-        print("No video information available. Cannot download.")
-        input("Press Enter to Continue...")
-        return
+            print(" Video downloaded successfully ".center(80, "="), end="\n\n")
+            input("Press Enter to Continue...")
 
-    try:
-        video_option = {
-            "format": "best",
-            "outtmpl": os.path.join(
-                os.path.expanduser("~/Downloads"), "%(title)s.%(ext)s"
-            ),
-            "quiet": True,
-            "no_warnings": True,
-        }
+        except yt_dlp.utils.DownloadError as e:
+            print(f"Download error occurred: {e}")
+            input("Press Enter to Continue...")
 
-        print(f"Downloading {video_info['title']} ...", end="\n\n")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            input("Press Enter to Continue...")
 
-        with yt_dlp.YoutubeDL(video_option) as video_download:
-            video_download.download([video_info["webpage_url"]])
+    def main(self):
 
-        print(" Video downloaded successfully ".center(80, "="), end="\n\n")
-        input("Press Enter to Continue...")
+        self.Welcome()
 
-    except yt_dlp.utils.DownloadError as e:
-        print(f"Download error occurred: {e}")
-        input("Press Enter to Continue...")
+        try:
+            video_url = input("Enter the URL of the Video >> ").strip()
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        input("Press Enter to Continue...")
+            if not video_url:
+                raise ValueError("URL cannot be empty.")
 
+            print(" Loading ... ".center(80, "=").title(), end="\n\n")
 
-def main():
+            info = self.get_info(self.video_url)
 
-    Welcome()
+            if info:
+                self.download_video(info)
 
-    try:
-        video_url = input("Enter the URL of the Video >> ").strip()
+        except ValueError as ve:
+            print(f"ValueError: {ve}")
+            input("Press Enter to Continue...")
 
-        if not video_url:
-            raise ValueError("URL cannot be empty.")
-
-        print(" Loading ... ".center(80, "=").title(), end="\n\n")
-
-        video_info = get_info(video_url)
-
-        if video_info:
-            download_video(video_info)
-
-    except ValueError as ve:
-        print(f"ValueError: {ve}")
-        input("Press Enter to Continue...")
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        input("Press Enter to Continue...")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            input("Press Enter to Continue...")
 
 
 if __name__ == "__main__":
-    main()
+    Video().main()
